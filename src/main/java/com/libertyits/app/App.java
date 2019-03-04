102bf2df8d88962bff4aec4f6f95704ea66dec90
tmcrae <taylor.mcrae@libertyits.com>
2019-03-04 12:57:02 -0500

    Initial commit with code base.

---
+++ src/main/java/com/libertyits/app/App.java
@@ -0,0 +1,13 @@
+package com.libertyits.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
